46f54fe361a0f0e8f3672fe5aed27699312c6329
Roger Hernandez <Roger@Rosas-MacBook-Pro.local>
2019-08-03 11:19:10 -0500

    updating app.js

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = express()
 const port = 3000
 
 app.get('/runpy', (req, res) => {
-    PythonShell.run('./scripts/script1.py', options, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        console.log('results: %j', results);
-        res.send(results);
-      });
+    // PythonShell.run('./scripts/script1.py', {}, function (err, results) {
+    //     if (err) throw err;
+    //     // results is an array consisting of messages collected during execution
+    //     console.log('results: %j', results);
+    //     res.send(results);
+    //   });
+
+      const exec = require('child_process').exec;
+        var yourscript = exec('python /scripts/script1.py',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
 })
 
 app.listen(port, () => {
