7188da7908ec0ed2df7bda6d663c623adfbe4f35
Roger Hernandez <Roger@Rosas-MacBook-Pro.local>
2019-08-03 10:45:06 -0500

    pushing first attempt at cycling targeted GPIO pins

---
+++ app.js
@@ -0,0 +1,8 @@
+//initial test of the pi-gpio boilerplate code
+var gpio = require("pi-gpio");
+ 
+gpio.open(16, "output", function(err) {		// Open pin 16 for output
+    gpio.write(16, 1, function() {			// Set pin 16 high (1)
+        gpio.close(16);						// Close pin 16
+    });
+});
