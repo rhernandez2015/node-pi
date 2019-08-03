1087811871460a364b7597975cf857fd9df69d3b
Roger Hernandez <Roger@Rosas-MacBook-Pro.local>
2019-08-03 10:49:00 -0500

    trying a thing

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ var gpio = require("pi-gpio");
 pinList = [2, 3, 4, 17, 27, 22, 10, 9]
 
 //function to set the passed GPIO pin to "high", then off
-function setPin(pinNumber){
-    gpio.open(pinNumber, "output", function(err) {		// Open pin 16 for output
-        gpio.write(pinNumber, 1, function() {			// Set pin 16 high (1)
-            setTimeout(function () {                    // Wait 5 seconds
-                gpio.close(pinNumber);					// Close the pin
-              }, 5000)
-        });
-    });
-};
+// function setPin(pinNumber){
+//     gpio.open(pinNumber, "output", function(err) {		// Open pin 16 for output
+//         gpio.write(pinNumber, 1, function() {			// Set pin 16 high (1)
+//             setTimeout(function () {                    // Wait 5 seconds
+//                 gpio.close(pinNumber);					// Close the pin
+//               }, 5000)
+//         });
+//     });
+// };
 
 //loop through all pins, cycle on and off
-pinList.forEach(function(pin){
-    setPin(pin);
-})
+// pinList.forEach(function(pin){
+//     setPin(pin);
+// })
 
+gpio.open(16, "output", function(err) {		// Open pin 16 for output
+    gpio.write(16, 1, function() {			// Set pin 16 high (1)
+        gpio.close(16);						// Close pin 16
+    });
+});
